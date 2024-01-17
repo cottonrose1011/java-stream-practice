4a472fa50eaa2977569a5e319e25e3e4e4a23d77
cottonrose1011 <pjeka7er43@gmail.com>
2024-01-17 20:35:33 +0900

    Solved Problems 11-15

--- src/main/java/problem/easy/Problem11.java
+++ src/main/java/problem/easy/Problem11.java
@@ -13,6 +13,7 @@ public class Problem11 {
      */
     public static Optional<Integer> findFirstGreaterThanTen(Set<Integer> numbers) {
         // 여기에 코드 작성
-        return Optional.empty();
+        return numbers.stream()
+                .filter(n -> n > 10).findFirst();
     }
 }
